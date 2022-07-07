825c2d127e8c090c43c67de189d2a3f40be8bdfb
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-07-07 11:52:22 +0900

    Add files via upload

---
+++ 백준17번.py
@@ -0,0 +1,6 @@
+a = int(input())
+
+
+for i in range(0,a):
+
+    print(" "* i +"*"*(2*(a-i)-1))
---
+++ 백준18번.py
@@ -0,0 +1,9 @@
+a = int(input())
+
+
+
+for i in range(a,0,-1):
+    print('*'*(a-i+1))
+
+for i in range(1,a):
+    print('*'*(a-i))
